4f59031b2b9af4c03e72dbe516a37c128ce167f3
Adam Jones <adam.jones@ucla.edu>
2018-01-31 18:39:32 -0800

    Added text uploading

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,49 +1,28 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
+let app = {
     socket: null,
 
-    // Application Constructor
     initialize: function() {
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
     },
 
-    // Update DOM on a Received Event
     receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+        if (id === "deviceready") {
+            this.socket = io('https://telestrations-csm117.herokuapp.com/');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+            let uploadText = document.getElementById('uploadText');
+            let textbox = document.getElementById('textbox');
+            let textinputdiv = document.getElementById('textinputdiv');
 
-        console.log('Received Event: ' + id);
-
-        socket = io('https://telestrations-csm117.herokuapp.com/');
+            uploadText.onclick = (e) => {
+                let text = textbox.value;
+                this.socket.emit('uploadText', text);
+                textinputdiv.innerHTML = '<p>Submitted!</p>';
+            };
+        }
     }
 };
 
