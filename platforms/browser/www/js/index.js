5a3de9f372a305918f8bdb3e98e4bb4426523e0e
Adam Jones <adam.jones@ucla.edu>
2018-03-06 00:46:04 -0800

    Picture sending

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -16,13 +16,38 @@ let app = {
             let uploadText = document.getElementById('uploadText');
             let textbox = document.getElementById('textbox');
 
+            let uploadPicture = document.getElementById('uploadPicture');
+            let canvas = document.getElementById('canvas');
+            let context = canvas.getContext('2d');
+
+            let clearCanvas = document.getElementById('clearCanvas');
+
             uploadText.onclick = (e) => {
                 let text = textbox.value;
                 this.socket.emit('uploadText', text);
             };
 
+            uploadPicture.onclick = (e) => {
+                let picture = canvas.toDataURL();
+                this.socket.emit('uploadPicture', picture);
+            };
+
+            clearCanvas.onclick = (e) => {
+                context.clearRect(0, 0, canvas.width, canvas.height);
+            };
+
             this.socket.on('downloadText', (text) => {
                 alert(text);
+            });
+
+            this.socket.on('downloadPicture', (picture) => {
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                
+                var img = new Image;
+                img.onload = function() {
+                    context.drawImage(img, 0, 0);
+                };
+                img.src = picture;
             });
         }
     }
